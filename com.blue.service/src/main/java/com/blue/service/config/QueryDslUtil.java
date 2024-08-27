c350bd7659da78fa0533ece437d88143004441c7
송형근 <soo1095@naver.com>
2024-08-27 23:33:01 +0900

    feat: QueryDsl 정렬 Util 추가

---
+++ com.blue.service/src/main/java/com/blue/service/config/QueryDslUtil.java
@@ -0,0 +1,60 @@
+package com.blue.service.config;
+
+import com.blue.service.domain.order.QOrder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.Expressions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j(topic = "QueryDslUtil")
+public class QueryDslUtil {
+
+    public static OrderSpecifier<?> getSortedColumn(Order order, Path<?> parent, String fieldName){
+        Path<Object> fieldPath = Expressions.path(Object.class, parent, fieldName);
+
+        return new OrderSpecifier(order, fieldPath);
+    }
+
+    public static List<OrderSpecifier> getAllOrderSpecifiers(Pageable pageable, String entityType){
+        List<OrderSpecifier> orders = new ArrayList<>();
+
+        if (!pageable.getSort().isEmpty()){
+            for(Sort.Order order : pageable.getSort()){
+                if("createdAt".equals(order.getProperty())){
+                    Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC;
+                    Path path;
+                    switch (entityType){
+                        case "order":
+                            path = QOrder.order;
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Entity의 타입이 잘못되었음");
+                    }
+                    OrderSpecifier<?> orderSpecifier = QueryDslUtil.getSortedColumn(direction, path, "createdAt");
+                    orders.add(orderSpecifier);
+                } else if ("modifiedAt".equals(order.getProperty())) {
+                    Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC;
+                    Path path;
+                    switch (entityType){
+                        case "order":
+                            path = QOrder.order;
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Entity의 타입이 잘못되었음");
+                    }
+                    OrderSpecifier<?> orderSpecifier = QueryDslUtil.getSortedColumn(direction, path, "modifiedAt");
+                    orders.add(orderSpecifier);
+                }
+
+            }
+        }
+
+        return orders;
+    }
+}
