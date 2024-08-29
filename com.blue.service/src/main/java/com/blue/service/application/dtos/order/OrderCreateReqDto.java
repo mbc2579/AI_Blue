ab7a0fe0340b9ce45a72b1ff5029e46ffb7fcda8
송형근 <soo1095@naver.com>
2024-08-29 19:37:53 +0900

    feat: Order - Store 연관관계 매핑 및 OrderProduct 구현

--- com.blue.service/src/main/java/com/blue/service/application/dtos/order/OrderCreateReqDto.java
+++ com.blue.service/src/main/java/com/blue/service/application/dtos/order/OrderCreateReqDto.java
@@ -1,12 +1,16 @@
 package com.blue.service.application.dtos.order;
 
+import com.blue.service.domain.Store;
 import com.blue.service.domain.order.Order;
+import com.blue.service.domain.order.OrderProductRequest;
 import com.blue.service.domain.order.OrderType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -16,12 +20,14 @@ import java.util.UUID;
 public class OrderCreateReqDto {
     private UUID storeId;
     private UUID destId;
+    private List<OrderProductRequest> orderProductRequests;
     private OrderType orderType;
 
-    public Order toOrder(String userName) {
+    public Order toOrder(String userName, Store store) {
         return Order.builder()
                 .orderId(UUID.randomUUID())
-                .storeId(this.storeId)
+                .store(store)
+                .orderProducts(new ArrayList<>())
                 .userName(userName)
                 .destId(this.destId)
                 .orderType(this.orderType)
---
+++ com.blue.service/src/main/java/com/blue/service/domain/order/OrderProduct.java
@@ -0,0 +1,35 @@
+package com.blue.service.domain.order;
+
+import com.blue.service.domain.BaseEntity;
+import com.blue.service.domain.Product;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity(name = "p_order_products")
+public class OrderProduct extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_product_id")
+    private UUID orderProductId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "product_quantity")
+    private Long productQuantity;
+}
