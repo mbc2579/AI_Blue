d51a22f18b21638bb986d23ebb62e0006c64bc4d
mbc2579 <mbc2579@naver.com>
2024-08-30 15:09:06 +0900

    코드 수정

--- com.blue.service/src/main/java/com/blue/service/domain/ProductRepository.java
+++ com.blue.service/src/main/java/com/blue/service/domain/ProductRepository.java
@@ -11,8 +11,6 @@ import java.util.UUID;
 public interface ProductRepository extends JpaRepository<Product, UUID> {
     // 삭제되지 않은 메뉴만 상세조회
     Optional<Product> findByProductIdAndDeletedAtIsNull(UUID productId);
-    // 삭제되지 않은 메뉴만 전체조회
-    List<Product> findAllByStore_StoreIdAndDeletedAtIsNullOrderByCreatedAt(UUID storeId);
     // isVisible이 true 이고 deletedAt가 null인 메뉴만 조회
     List<Product> findAllByStore_StoreIdAndDeletedAtIsNullAndIsVisibleTrueOrderByCreatedAt(UUID storeId);
 }
