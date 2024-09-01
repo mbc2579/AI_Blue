581a8110466d2865fc19cb72a745d64c600e5761
MeGuuuun <pjm991012@gmail.com>
2024-09-01 22:15:42 +0900

    jwtAuthorizationFilter GlobalFilter로 다시 구현

--- com.blue.gateway/src/main/java/com/blue/gateway/config/JwtAuthorizationFilter.java
+++ com.blue.gateway/src/main/java/com/blue/gateway/config/JwtAuthorizationFilter.java
@@ -3,28 +3,24 @@ package com.blue.gateway.config;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.Cookie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 import org.springframework.util.StringUtils;
-import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-import jakarta.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
 import java.security.Key;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 
 @Slf4j(topic = "JWT 검증 및 인가")
-public class JwtAuthorizationFilter extends OncePerRequestFilter {
+public class JwtAuthorizationFilter implements GlobalFilter{
 
     private PathMatcher pathMatcher = new AntPathMatcher();
 
@@ -43,32 +39,45 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getPath().toString();
 
-        String path = req.getServletPath();
 
-        if(isExcludeUrl(path)){
-            filterChain.doFilter(req, res);
-            return;
+        if (isExcludeUrl(path)) {
+            return chain.filter(exchange);
         }
 
-        String tokenValue = getTokenFromRequest(req);
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
 
-        if(StringUtils.hasText(tokenValue)){
-            tokenValue = substringToken(tokenValue);
-            log.info(tokenValue);
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+            String jwt = authHeader.substring(7);
 
-            if(!validateToken(tokenValue)){
-                log.error("Token Error");
-                return;
+            try {
+                Jwts.parserBuilder()
+                        .setSigningKey(secretKey)
+                        .build()
+                        .parseClaimsJws(jwt);
+
+                return chain.filter(exchange);
+            } catch (ExpiredJwtException ex) {
+                log.error("JWT token has expired");
+            } catch (UnsupportedJwtException ex) {
+                log.error("Unsupported JWT token");
+                return Mono.error(new JwtException("Unsupported JWT token", ex));
+            } catch (MalformedJwtException ex) {
+                log.error("Malformed JWT token");
+                return Mono.error(new JwtException("Malformed JWT token", ex));
+            } catch (SignatureException ex) {
+                log.error("Invalid JWT signature");
+                return Mono.error(new JwtException("Invalid JWT signature", ex));
+            } catch (IllegalArgumentException ex) {
+                log.error("Invalid JWT token");
+                return Mono.error(new JwtException("Invalid JWT token", ex));
             }
         }
 
-        Claims info = getUserInfoFromToken(tokenValue);
-
-        res.addHeader("X-User-Name", info.getSubject());
-
-        filterChain.doFilter(req, res);
+        log.error("Authorization header not found");
+        return Mono.error(new Exception("Authorization header not found"));
     }
 
     private boolean isExcludeUrl(String path) {
@@ -78,50 +87,6 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
             }
         }
         return false;
-    }
-
-    public String getTokenFromRequest(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if(cookies != null) {
-            for(Cookie cookie : cookies) {
-                if(cookie.getName().equals("Authorization")) {
-                    try {
-                        return URLDecoder.decode(cookie.getValue(), "UTF-8");
-                    } catch (UnsupportedEncodingException e) {
-                        return null;
-                    }
-                }
-            }
-        }
-        return null;
-    }
-
-    public String substringToken(String tokenValue) {
-        if (StringUtils.hasText(tokenValue) && tokenValue.startsWith("Bearer")) {
-            return tokenValue.substring(7);
-        }
-        logger.error("Not Found Token");
-        throw new NullPointerException("Not Found Token");
-    }
-
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-            return true;
-        } catch (SecurityException | MalformedJwtException | SignatureException e) {
-            logger.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
-        } catch (ExpiredJwtException e) {
-            logger.error("Expired JWT token, 만료된 JWT token 입니다.");
-        } catch (UnsupportedJwtException e) {
-            logger.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
-        } catch (IllegalArgumentException e) {
-            logger.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
-        }
-        return false;
-    }
-
-    public Claims getUserInfoFromToken(String token) {
-        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
     }
 
 }
