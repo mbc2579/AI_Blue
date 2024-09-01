dadf82e2091dd6aa0de0bc2d46499e4c3c4e548c
MeGuuuun <pjm991012@gmail.com>
2024-09-01 23:42:39 +0900

    배송지 도메인 기능 구현 완료

--- com.blue.service/src/main/java/com/blue/service/controller/DestinationController.java
+++ com.blue.service/src/main/java/com/blue/service/controller/DestinationController.java
@@ -3,12 +3,16 @@ package com.blue.service.controller;
 import com.blue.service.application.DestinationService;
 import com.blue.service.application.dtos.destination.DestinationReqDto;
 import com.blue.service.application.dtos.destination.DestinationResDto;
+import com.blue.service.domain.destination.Destination;
+import com.blue.service.domain.destination.DestinationRepository;
+import feign.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -24,11 +28,27 @@ public class DestinationController {
         return ResponseEntity.ok(destinationService.addDestination(requestDto, userName));
     }
 
-    public ResponseEntity<Boolean> editDestination(
+    @GetMapping
+    public ResponseEntity<List<DestinationResDto>> searchDestinations(@RequestHeader(name="X-User-Name", required=false) String userName) {
+        return ResponseEntity.ok(destinationService.searchDestination(userName));
+    }
+
+    @GetMapping("/{destId}")
+    public ResponseEntity<DestinationResDto> getDestination(@PathVariable("destId") UUID destId, @RequestHeader(name="X-User-Name", required = false) String userName) {
+        return ResponseEntity.ok(destinationService.getDestination(destId, userName));
+    }
+
+    @PutMapping("/{destId}")
+    public ResponseEntity<DestinationResDto> editDestination(
             @RequestBody DestinationReqDto requestDto,
-            @PathVariable(name="destId") UUID destId,
-            @RequestHeader(name="X-User-Name", required = false) String userName) {
-        return ResponseEntity.ok(true);
+            @PathVariable(name="destId") UUID destId) {
+
+        return ResponseEntity.ok(destinationService.editDestination(requestDto, destId));
+    }
+
+    @DeleteMapping("/{destId}")
+    public ResponseEntity<DestinationResDto> deleteDestination(@PathVariable(name="destId") UUID destId, @RequestHeader(name="X-User-Name", required = false) String userName){
+        return ResponseEntity.ok(destinationService.deleteDestination(destId, userName));
     }
 
 
