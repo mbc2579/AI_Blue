10d7b3080c55efd9d292d27ad57e2ac7c8cb6b6e
송형근 <soo1095@naver.com>
2024-09-02 23:21:50 +0900

    feat: User Role Caching 추가

--- com.blue.gateway/src/main/java/com/blue/gateway/infrastructure/AuthClient.java
+++ com.blue.gateway/src/main/java/com/blue/gateway/infrastructure/AuthClient.java
@@ -1,6 +1,7 @@
 package com.blue.gateway.infrastructure;
 
 import com.blue.gateway.AuthService;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "auth")
 public interface AuthClient extends AuthService {
 
+    @Cacheable(cacheNames = "userRoleCache", key = "args[0]")
     @GetMapping("/api/auth/authority")
     String getAuthority(@RequestParam(value = "userName") String userName);
 }
